2326ce1a671c191c412d91f2e69d13db724b136a
Bi4eps <smovzhmykyta@gmail.com>
2023-09-22 23:28:53 +0300

    ref: class UserStory

--- src/main/java/com/epam/rd/autotasks/sprintplanning/tickets/UserStory.java
+++ src/main/java/com/epam/rd/autotasks/sprintplanning/tickets/UserStory.java
@@ -12,16 +12,16 @@ public class UserStory extends Ticket {
     public void complete() {
         boolean temp = true;
         for (UserStory depend : dependencies) {
-            System.out.println("from complete - " + depend.isCompleted()); //
-            if (!depend.isCompleted()) temp = false;
+            if (!depend.isCompleted()) {
+                temp = false;
+                break;
+            }
         }
 
         isTicketCompleted = temp;
     }
 
-    public UserStory[] getDependencies() {
-        return dependencies.clone();
-    }
+    public UserStory[] getDependencies() { return dependencies.clone(); }
 
     @Override
     public String toString() {
